1e7ef126dbdc773aa425bfa0b2644d8859782429
SuslikV <vx2010@i.ua>
2023-11-10 14:19:06 +0200

    Change name to default

--- Samples/ApplicationLoopback/proc_capture.cmd
+++ Samples/ApplicationLoopback/proc_capture.cmd
@@ -5,6 +5,6 @@ REM Look for first executable file that match the filename and get its PID
 for /f "tokens=2" %%a in ('tasklist /nh /fi "imagename eq %app%"') do if not [%%a]==[No] (set process_pid=%%a & goto capture)
 
 :capture
-ApplicationLoopback_EnterToStopCFG %process_pid% includetree Captured.wav 48000 32 2 3
+ApplicationLoopback %process_pid% includetree Captured.wav 48000 32 2 3
 
 pause
